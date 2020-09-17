ad422e8f76ff599fc1454cfd133ef965594b2357
drogan120 <alimahmudin.hp@gmail.com>
2020-09-17 21:30:31 +0700

    adding fontawesome and test file

--- index.php
+++ index.php
@@ -6,10 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Jasa Anti Rayap Pest Control Pembasmi Hama Jakarta | Fumida</title>
     <link rel="stylesheet" href="fumida_admin/src/css/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="fumida_admin/src/css/fontawesome/css/all.css">
 </head>
 
 <body>
-    <h1 class="text-secondary">hello</h1>
+
 
     <script src="fumida_admin/src/js/jquery/jquery-3.5.1.js"></script>
     <script src="fumida_admin/src/js/popper/popper.min.js"></script>
