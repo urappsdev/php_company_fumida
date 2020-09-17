7a2e83bfc51a38712bf95d2e28985b6c69046e6a
drogan120 <alimahmudin.hp@gmail.com>
2020-09-17 20:46:29 +0700

    folder sctructure

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jasa Anti Rayap Pest Control Pembasmi Hama Jakarta | Fumida</title>
+    <link rel="stylesheet" href="fumida_admin/src/css/bootstrap/bootstrap.min.css">
+</head>
+
+<body>
+    <h1 class="text-secondary">hello</h1>
+
+    <script src="fumida_admin/src/js/jquery/jquery-3.5.1.js"></script>
+    <script src="fumida_admin/src/js/popper/popper.min.js"></script>
+    <script src="fumida_admin/src/js/bootstrap/bootstrap.min.js"></script>
+</body>
+
+</html>
